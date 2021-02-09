1cc68658323d5c10e45883edc6287e7712270b95
Oly Erofeeva <o.leland@yandex.ru>
2021-02-09 21:02:10 +0300

    ui kit: bookmark button - refactor

--- src/components/ui/BookmarkButton/BookmarkButton.js
+++ src/components/ui/BookmarkButton/BookmarkButton.js
@@ -1,36 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './BookmarkButton.css';
 
-function BookmarkButton({ bookmarkState }) {
-  if (bookmarkState === 'hovered') { // TODO: refactor. maybe switch svg/path only
-    return (
-      <button className="bookmark-button" type="button">
-        <svg width="14" height="19" viewBox="0 0 14 19" fill="none">
-          <path
-            stroke="#1A1B22"
-            strokeWidth="2"
-            d="M6.38218 12.7137L1 16.9425V1L13 1V16.9425L7.61782 12.7137L7 12.2283L6.38218 12.7137Z"
-          />
-        </svg>
-      </button>
-    );
-  }
+import BookmarkIcon from '../../svg/BookmarkIcon/BookmarkIcon';
 
-  if (bookmarkState === 'marked') {
-    return (
-      <button className="bookmark-button" type="button">
-        <svg width="14" height="19" viewBox="0 0 14 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0 1C0 0.447715 0.447715 0 1 0H13C13.5523 0 14 0.447715 14 1V19L7 13.5L0 19V1Z" fill="#2F71E5" />
-        </svg>
-      </button>
-    );
-  }
+function BookmarkButton({
+  className = '',
+  isSelected = false,
+}) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
 
   return (
-    <button className="bookmark-button" type="button">
-      <svg width="14" height="19" viewBox="0 0 14 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M6.38218 12.7137L1 16.9425V1L13 1V16.9425L7.61782 12.7137L7 12.2283L6.38218 12.7137Z" stroke="#B6BCBF" strokeWidth="2" />
-      </svg>
+    <button
+      className={`bookmark-button ${className}`}
+      type="button"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <BookmarkIcon isHovered={isHovered} isSelected={isSelected} />
     </button>
   );
 }
