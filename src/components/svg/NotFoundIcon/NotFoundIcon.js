136a62bd225bda7f19a0aac004fc8d82fad1ae00
Oly Erofeeva <o.leland@yandex.ru>
2021-02-11 21:58:37 +0300

    svg: not found icon

---
+++ src/components/svg/NotFoundIcon/NotFoundIcon.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function NotFoundIcon() {
+  return (
+    <svg
+      width="96"
+      height="96"
+      viewBox="0 0 96 96"
+      fill="none"
+    >
+      <circle
+        stroke="#d1d2d6"
+        cx="43"
+        cy="43"
+        r="36.5"
+      />
+      <path stroke="#d1d2d6" d="M69 69L88.5 88.5" />
+      <path stroke="#d1d2d6" d="M58.3283 55.959C54.6606 51.6979 49.2275 48.9998 43.1642 48.9998C37.1009 48.9998 31.6678 51.6979 28 55.959" />
+      <circle fill="#d1d2d6" cx="55.5" cy="33.5" r="1.5" />
+      <circle fill="#d1d2d6" cx="30.5" cy="33.5" r="1.5" />
+    </svg>
+  );
+}
+
+export default NotFoundIcon;
